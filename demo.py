fea339ceff8c58508393e498be91e1874c566027
Sarmiento28 <91348772+Sarmiento28@users.noreply.github.com>
2021-10-05 21:56:30 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,45 +1,16 @@
-import random
-# Demo of basic input in python
-# name = input("Enter your name: ")
-# print("Hello " + name)
-
-# Demo of a program that computes for an area of a circle
-# radius = float(input("Enter the radius: "))
-# area = 3.14 * (radius**2)
-# print("The area of the Circle is: " + str(area))
-
-# Demo of a program that computes for a volume of a cylinder
-# radius = float(input("Enter the Radius: "))
-# height = float(input("Enter the Height: "))
-# volume = 3.14 *(radius**2 * height)
-# print("The Volume of the Cylinder: " + str(volume))
-
-#Demo for array 
-# fruits = ["apple", "banana", "cherry", "strawberry", "melon", "watermelon"]
-# print(fruits[0])
-# print(fruits[1])
-# print(fruits[2])
-
-#Demo for for Loop
-# for x in fruits:
-#     print(x)
-
-# i = 0
-# while i < 10:
-#     print("Hello World")
-#     print(i)
-#     i = i + 1
-
-
 
 #sample text-based game with simple conditional statement
 character_health = 100
+buzz_health = 120
+crak_health = 110
 item = ""
+import random
 
 name = input("Enter your name: ")
 print("Hello " + name)
 
 while character_health > 0:
+
     if character_health != 100 and item == "Fish":
         n = input("You have taken some damage to heal select [1 to heal/ 0 to ignore]")
         if n == "1":
@@ -48,6 +19,14 @@ while character_health > 0:
         else :
              print("Your character's health is back to " + str(character_health))
 
+    elif  character_health != 100 and item == "Athena Shield":
+                n = input("You have chance to heal!! using your:" + item + "! \n" + "[press 1 to heal]")
+                if n == "1":
+                    character_health += 100
+                    print("Character health + 100! \n" + "Your Character Health is now: " + str(character_health))
+
+
+  
 
     v = int(input("Choose 1 if you want to cross the river\nChoose 2 if you want to jump on the ravine\nChoose 3 if you want to fight monster in the dungeon \nInput: "))
    
@@ -56,7 +35,7 @@ while character_health > 0:
         choice = input("If you want to go fishing select [1 for yes/ 0 for no]")
         if choice == "1":
             #Fishing minigame
-            print("You have chosen Fishing! ")
+            print("You have chosen fish! ")
             chance = random.randint(0,9)
             if chance > 6:
                 item = "Fish"
@@ -73,9 +52,65 @@ while character_health > 0:
         print("You have jumped into the ravine")
         print("Your character has taken 10pt of damage")
         character_health = character_health - 10
+
     elif v == 3:
+    
         #damages the player
         print("You have enter the dungeon")
+        #monster attack
+        chance = random.randint(0,9)
+        if chance > 5:
+            
+            print ("A Crak Appeared !!!")
+            choice = input("Fight the The Crack! \n \t [1 to fight using your Basic attack and 0 for to fight using your Skills]")
+            if choice == "1":
+                print ("You dealt normal damage using your basic attack! Crak taken -20hp")
+                print ("Your Character taken some damage from the Crak! -20")
+                character_health -= 20
+                crak_health -= 20
+                print ("crak Health: " + str (crak_health))
+            elif choice == "0":
+                print ("You dealt critical damages using your Special Skill attack! Crak taken -60hp")
+                print ("Your Character taken some damage from the Crak! -20")
+                character_health -= 20
+                crak_health -= 60
+                print ("crak Health: " + str (crak_health))
+
+            if crak_health <= 0:
+                print("you have sucessfully deafeated the Crak! and gain new item! ")
+                item = "Haas claw"
+                print("You have found " + item + "!\n")
+               
+        else:
+            print ("An buzz appeared!")
+            choice =  input("Fight the Buzz! \n \t [1 to Fight using your Axe, or 0 to Fight using your Sword]")
+            if choice == "1":
+                print ("you faught using your axe! Buzz taken -50hp")
+                print ("your character taken some damage from the attack of the Buzz! -30hp")
+                character_health -= 30
+                buzz_health -= 50
+                print ("Buzz Health: " + str (buzz_health))
+            elif choice == "0":
+                item = "Smooth Love Potion"
+                print ("you fought using your axe! Buzz taken -20hp")
+                print ("your character taken some damages from the attack of the Buzz! -30hp")
+                character_health -= 30
+                buzz_health -= 20
+                print("Buzz Health:" + str(buzz_health))
+
+                
+            if buzz_health <= 0:
+                print("you have sucessfully deafeated the Buzz! and gain new item! ")
+                item = "Athena Shield"
+                print ("You have found " + item + "! \n")
+                print ("Your character is leveling up!")
+
+           
+
+                
+
+           
+        
 
     else:
         print("Invalid input")
@@ -83,10 +118,3 @@ while character_health > 0:
     print("Your character's Health: " + str(character_health))
 
 print("Your Character is dead!")
-
-
-
-
-
-
-
